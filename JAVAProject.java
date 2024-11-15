83b9941b097c97c2bb0eb0a5ab9011a07ed767d1
LyndaFitz <lyndaa.fitz@gmail.com>
2024-11-15 16:20:12 -0500

    NEW JAVA PROJECT

---
+++ JAVAProject.java
@@ -0,0 +1,17 @@
+
+public class JAVAProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+//This program prints Welcome to Java! 
+
+public class Welcome {	
+public static void main(String[] args) { 
+ System.out.println("Welcome to Java!");
+}
+}
+
